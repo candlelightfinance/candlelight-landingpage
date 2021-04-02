cee507013c45dc72c087a73c5f37433554208abc
candlelightfinance <81801032+candlelightfinance@users.noreply.github.com>
2021-04-02 12:04:30 +0200

    Create MenuItems.js

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Telegram',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Github',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Medium',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Enter Dapp',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
